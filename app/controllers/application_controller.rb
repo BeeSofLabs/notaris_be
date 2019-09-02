fb05f03b650a27ad72a223fb429c555c2234b414
MusaAbdillah <musa@clappingape.com>
2019-09-02 22:52:05 +0700

    make authentication every request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   include ExceptionHandler
   protect_from_forgery unless: -> { request.format.json? }
 
-	
-  def hello
-  	json_response({message: "Hello There!"})
-  end
+	# called before every action on controller
+	before_action :authorize_request
+	skip_before_action :authorize_request, only: :hello
+	attr_reader :current_user
+
+	def hello
+		json_response({message: "Hello There!"})
+	end
+
+	private
+		def authorize_request
+			@current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+		end
 end
