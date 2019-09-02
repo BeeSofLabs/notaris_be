c9cce4413aeae4073af42528623db4e27ea75451
MusaAbdillah <musa@clappingape.com>
2019-09-02 22:34:26 +0700

    protect from forgery unless reponse json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   include Response
   include ExceptionHandler
+  protect_from_forgery unless: -> { request.format.json? }
 
+	
   def hello
   	json_response({message: "Hello There!"})
   end
