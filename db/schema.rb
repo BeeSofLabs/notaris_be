b872f6ded66d1aec12fd6bcecb633d57921d72ae
MusaAbdillah <musa@clappingape.com>
2019-09-23 22:46:38 +0700

    add approved column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_150408) do
+ActiveRecord::Schema.define(version: 2019_09_23_154552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_150408) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false
   end
 
 end
