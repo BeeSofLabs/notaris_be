546c08045c63c848345f53e403e1a5372624b8ec
MusaAbdillah <musa@clappingape.com>
2019-09-25 23:01:16 +0700

    print schema at every single model

--- test/models/users_role_test.rb
+++ test/models/users_role_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: users_roles
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  role_id    :bigint(8)
+#  user_id    :bigint(8)
+#
+# Indexes
+#
+#  index_users_roles_on_role_id  (role_id)
+#  index_users_roles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (role_id => roles.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 require 'test_helper'
 
 class UsersRoleTest < ActiveSupport::TestCase
