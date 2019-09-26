d9c426f029afbb7302b3d66af8125707b1232377
MusaAbdillah <musa@clappingape.com>
2019-09-26 23:45:39 +0700

    make registration and status request

--- app/services/privy.rb
+++ app/services/privy.rb
@@ -1,28 +1,64 @@
 class Privy
 	attr_reader :email, 
 				:phone, 
-				:selfie, 
-				:ktp,
+				:selfie_image, 
+				:identity_image,
 				:identity,
 				:name,
-				:dob
+				:dob,
+				:token
 
 
 	def initialize(opts)
 		@email 		= opts[:email]
 		@phone 		= opts[:phone]
-		@selfie 	= opts[:selfie] 
-		@ktp 		= opts[:ktp]
+		@selfie_image 	= opts[:selfie_image] 
+		@identity_image	= opts[:identity_image]
 		@identity 	= opts[:identity]
 		@name 		= opts[:name]
-		@dob 		= opts[:dob]
+		@dob 		= opts[:dob],
+		@token		= opts[:token]
+	end
+
+	def headers
+		headers = {
+			"Authorization": "Basic " + Base64.strict_encode64("#{ENV["PRIVY_USERNAME"]}:#{ENV["PRIVY_PASSWORD"]}"),
+			"Merchant-Key": ENV["MERCHANT_KEY"],
+			"Content-Type": "multipart/form-data"
+		}
 	end
 
 	def registration
 		# post data to Privy here!
+		body = {
+			email: email,
+			phone: phone,
+			selfie: selfie_image,
+			ktp: identity_image,
+			identity: {
+				"nik": identity,
+				"nama": name,
+				"tanggalLahir": dob
+			}
+		}
+
+		data = HTTParty.post(
+			"#{ENV["PRIVY_REGISTRATION_URL"]}",
+			body: body,
+			headers: headers
+		)
 	end
 
 	def registration_status
 		# check registration status goes here!
+		body = {
+			"token": token
+		}
+		data = HTTParty.post(
+			"#{ENV["PRIVY_REGISTRATION_STATUS_URL"]}",
+			body: body,
+			headers: headers
+		)
 	end
+
 end
