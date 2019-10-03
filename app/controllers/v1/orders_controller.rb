685aebe04e433ed1550dc521681ab21f34f94806
MusaAbdillah <musa@clappingape.com>
2019-10-03 08:25:58 +0700

    create basic order method

--- app/controllers/v1/orders_controller.rb
+++ app/controllers/v1/orders_controller.rb
@@ -1,5 +1,32 @@
 class V1::OrdersController < ApplicationController
+  before_action :find_notary, only: [:create]
+  before_action :find_notary_service, only: [:create]
+
+
   def create
-  	json_response({message: "order created!"})
+  	order = current_user.orders.create!(
+  		order_type: params[:order_type],
+  		notary_id: @notary.id,
+  		grand_total: @notary_service.price
+	)
+  	json_response({message: "order created!", order: order}, :created)
   end
+
+  private
+
+  	def find_notary
+  		@notary = User.find(params[:notary_id])
+  	end
+
+  	def find_notary_service
+  		@notary_service = @notary.notary_services.find(params[:notary_service_id])
+  	end
+
+	def order_params
+		params.require(:order).permit(
+			:order_type,
+			:notary_id,
+			:notary_service_id
+		)
+	end
 end
