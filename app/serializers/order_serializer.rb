d5987baee007b7c6e996afb6a94d57b4633ff5a8
MusaAbdillah <musa@clappingape.com>
2019-10-10 06:09:25 +0700

    updat order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,11 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes 	:id,
+				:grand_total,
+				:order_type,
+				:status,
+				:created_at,
+				:updated_at,
+				:notary_id,
+				:user_id
+  has_one :fidusia_order
 end
