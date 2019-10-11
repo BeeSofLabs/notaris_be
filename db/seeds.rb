3c32edc4a954e13d1dc8be20ff4ab7ff52829b9a
MusaAbdillah <musa@clappingape.com>
2019-10-11 23:07:15 +0700

    seed role

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.create(
+	[
+		{name: "notaris"},
+		{name: "debitur"},
+		{name: "kreditur"},
+		{name: "bpn"},
+		{name: "collateral owner"},
+		{name: "admin"}
+	]
+)
