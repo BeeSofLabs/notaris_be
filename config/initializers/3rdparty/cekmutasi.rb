b5bef5e71035f9bd78a0e51b8efa36269a537682
ary <ary@clappingape.com>
2019-11-03 11:47:31 +0700

    add 3rdparty integration with cekmutasi and privy

---
+++ config/initializers/3rdparty/cekmutasi.rb
@@ -0,0 +1,27 @@
+module CekMutasiModule
+    # Bank
+    def self.bank_mutation
+        
+    end
+
+    def self.bank_transfer
+
+    end
+
+    # OVO
+    def self.ovo_mutation
+
+    end
+
+    def self.ovo_transfer
+    end
+
+    # GOPAY
+    def self.gopay_mutation
+
+    end
+
+    def self.gopay_transfer
+
+    end
+end
---
+++ config/initializers/3rdparty/privy.rb
@@ -0,0 +1,17 @@
+module PrivyModule
+    def self.registration
+        
+    end
+
+    def self.status_of_registration
+
+    end
+
+    def self.upload_document
+
+    end
+
+    def self.check_document_status
+        
+    end
+end
