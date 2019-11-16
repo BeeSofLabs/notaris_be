a4a8ba9b343de38512b5ac48d9fd46b4f2ebdb22
ian ogawa <alfian@clappingape.com>
2019-11-17 00:23:13 +0700

    update gemfile lock fixed migrate coordinate

--- db/migrate/20191113013658_change_type_coordinate.rb
+++ db/migrate/20191113013658_change_type_coordinate.rb
@@ -1,6 +1,6 @@
 class ChangeTypeCoordinate < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :lng, :float
-    change_column :users, :lat, :float
+    change_column :users, :lng, :float, using: 'lng::float'
+    change_column :users, :lat, :float, using: 'lat::float'
   end
 end
