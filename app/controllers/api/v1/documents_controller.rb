6860b6f11113f12f009f98873592b9c550603291
ary <ary@clappingape.com>
2019-11-28 15:36:08 +0700

    move to public/document

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DocumentsController < ApplicationController
 
     def show
-        temp_path = "tmp/document/"
+        temp_path = "public/document/"
         case(params[:doctype])
         when 'skmht'
             temp_path += "skmht_template.html"
@@ -10,10 +10,12 @@ class Api::V1::DocumentsController < ApplicationController
         when 'covernote'
             temp_path += "covernote_template.html"
         when 'fidusia'
-            temp_path += "fidusia_template. html"
+            temp_path += "fidusia_template.html"
+        else
+            return json_response({message: "invalid document type"}, :not_found)
         end
 
-        data = File.read(temp_path).gsub("\n", '')
+        data = File.read(temp_path).gsub("\n", '').strip!
         json_response(  {tipe: params[:doctype], template: data}, :ok)
 
     end
