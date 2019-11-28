7c699ca4c885b4878d9b4e853021df1801c5eac9
ary <ary@clappingape.com>
2019-11-28 16:23:24 +0700

    fix move logic into model

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -1,22 +1,13 @@
 class Api::V1::DocumentsController < ApplicationController
 
     def show
-        temp_path = "public/document/"
-        case(params[:doctype])
-        when 'skmht'
-            temp_path += "skmht_template.html"
-        when 'apht'
-            temp_path += "apht_template.html"
-        when 'covernote'
-            temp_path += "covernote_template.html"
-        when 'fidusia'
-            temp_path += "fidusia_template.html"
-        else
-            return json_response({message: "invalid document type"}, :not_found)
-        end
+        content = ::Document.content_template(params[:doctype])
 
-        data = File.read(temp_path).gsub("\n", '').strip!
-        json_response(  {tipe: params[:doctype], template: data}, :ok)
+        unless content
+            return json_response({message: "invalid document type"}, :not_found)
+        else
+            json_response(  {tipe: params[:doctype], template: content}, :ok)
+        end
 
     end
 
