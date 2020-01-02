592d361a909d08b3b1958d572ad5b0d4d66522de
ary <ary@clappingape.com>
2020-01-02 16:11:58 +0700

    add notification message in every action

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::AuthenticationController < ApplicationController
 	skip_before_action :authorize_request, only: :authenticate
 	def authenticate
 		user_auth = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-		
-		Notification.build("notif_submission", user_auth[:user][:id], "Akun anda telah login")
+	
 		json_response(user_auth)
 	end
 
